b433b92157a7683e2bc45f7dd0f27793541b5c4c
张啸宇 <zjupeterzhang@gmail.com>
2020-06-05 16:06:46 +0800

    fist commit

---
+++ pages/index/index.vue
@@ -0,0 +1,115 @@
+<template>
+	<view class="content">
+		<!-- <open-data type="userAvatarUrl" class="user"></open-data> -->
+		<!-- top -->
+		<view class="userInfo">
+
+		</view>
+		<!-- list -->
+		<view class="lists">
+			<view class="kitchen" @click="gotoKitchen">
+				<image src="/static/index/order.png" alt="">
+			</view>
+			<view class="travel" @click="gotoTravel">
+				<image src="/static/index/travel.png" alt="">
+			</view>
+			<view class="love" @click="checkLoveStory">
+				<image src="/static/index/love.png" alt="">
+			</view>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			// uni.authorize({
+			//     scope: 'scope.userInfo',
+			//     success() {
+			//         console.log(1)
+			//     }
+			// })
+			// uni.getUserInfo({
+			// 	success(res) {
+			// 		console.log(res)
+			// 	}
+			// })
+			// wx.cloud.init({
+			//   env: 'miaomiaowudev-645d34'
+			// })
+			// const db = wx.cloud.database()
+			// db.collection('todos').add({
+			//   // data 字段表示需新增的 JSON 数据
+			//   data: {
+			//     // _id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
+			//     description: "learn cloud database",
+			//     due: new Date("2018-09-01"),
+			//     tags: [
+			//       "cloud",
+			//       "database"
+			//     ],
+			//     // 为待办事项添加一个地理位置（113°E，23°N）
+			//     location: new db.Geo.Point(113, 23),
+			//     done: false
+			//   },
+			//   success: function(res) {
+			//     // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+			//     console.log(res)
+			//   }
+			// })
+		},
+		methods: {
+			gotoKitchen() {
+				uni.navigateTo({
+					url: '/pages/kitchen/kitchen'
+				})
+			},
+			gotoTravel() {
+				uni.showModal({
+					content: '功能正在开发，敬请期待！',
+					showCancel: false,
+					confirmText: '好哒！',
+				})
+			},
+			checkLoveStory() {
+				uni.showModal({
+					content: '功能正在开发，敬请期待！',
+					showCancel: false,
+					confirmText: '好哒！',
+				})
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	view.userInfo {
+		width: 100%;
+		height: 385rpx;
+		background-color: #339df5;
+	}
+
+	view.lists {
+		margin-top: 20rpx;
+		width: 740rpx;
+	}
+
+	view.kitchen,
+	view.travel,
+	view.love {
+		width: 100%;
+	}
+
+	view.kitchen>image,
+	view.travel>image,
+	view.love>image {
+		width: 100%;
+		height: 292rpx;
+	}
+</style>
---
+++ pages/kitchen/kitchen.vue
@@ -0,0 +1,91 @@
+<template>
+	<view>
+		<!-- 侧边栏 -->
+		<scroll-view class="sidebar" :style="{height: appHeight}">
+			<view class="iterms">
+				<view @click="chooseFoodSort(index)" v-for="(item,index) in foodSorts" :key="index" class="iterm" :class="[item.className,chosenFoodSortIndex==index?chosen:'']">
+					{{item.name}} 
+				</view>
+
+			</view>
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		created() {
+			this.appHeight = uni.getSystemInfoSync().windowHeight + 'px'
+		},
+		data() {
+			return {
+				appHeight: '', // app高度
+				foodSorts: [{
+						'name': '中餐',
+						'className': 'ChineseFood',
+						'food': [
+
+						]
+					},
+					{
+						'name': '饮料',
+						'className': 'drink'
+					},
+					{
+						'name': '西餐',
+						'className': 'beef'
+					}
+				],
+				chosenFoodSortIndex: 0
+			};
+		},
+		methods: {
+			chooseFoodSort(chosenIndex) {
+				this.chosenFoodSortIndex = chosenIndex
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	scroll-view.sidebar {
+		width: 35%;
+		height: 100%;
+		/* background-color: yellow */
+	}
+
+	scroll-view.sidebar .iterms {
+		width: 100%;
+
+	}
+
+	scroll-view.sidebar .iterms .iterm {
+		/* text-align: center; */
+		height: 100rpx;
+		line-height: 100rpx;
+		padding-left: 100upx;
+		font-size: 30upx;
+		color: #333;
+		background-color: rgb(248, 248, 248);
+		/* border-bottom: 1upx solid #6d91fe; */
+	}
+
+	.sidebar .chosen {
+		background-color: #fff !important;
+	}
+
+	.ChineseFood {
+		background: url(../../static/kitchen/noodle.png) no-repeat 10rpx center;
+		background-size: 50upx;
+	}
+
+	.drink {
+		background: url(../../static/kitchen/drink.png) no-repeat 10rpx center;
+		background-size: 50upx;
+	}
+
+	.beef {
+		background: url(../../static/kitchen/beef.png) no-repeat 10rpx center;
+		background-size: 50upx;
+	}
+</style>
